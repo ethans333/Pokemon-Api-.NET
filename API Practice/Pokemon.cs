96512b5abe4cac0e625c9c56fd95b7b7f57b79b8
Ethan <gamesniper99@live.com>
2024-03-29 12:21:34 -0400

    Add project files.

---
+++ API Practice/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Practice
+{
+    internal class Pokemon
+    {
+        private int maxMovesDisplay = 5;
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<string> moves { get; set; }
+        public int weight { get; set; }
+        public int height { get; set; }
+
+        public Pokemon(int id, string name, List<string> moves, int weight, int height)
+        {
+            this.id = id;
+            this.name = name;
+            this.moves = moves;
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public void print() {
+            Console.WriteLine("---------------------\n");
+            Console.WriteLine("Pokemon: {0}\nWeight: {1}\nHeight: {2}\nMoves:", this.name, this.weight, this.height);
+
+            int i;
+
+            for (i = 0; i < maxMovesDisplay; i++) {
+                if (i > this.moves.Count) 
+                    break;
+                Console.WriteLine("\t* {0}", this.moves[i]);
+            }
+
+            if (i < this.moves.Count) 
+                Console.WriteLine("\tand {0} others...", this.moves.Count - i);
+
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ API Practice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+
+// Pokemon p = new Pokemon(123, "Pikachu", new List<string>(), 33);
+
+namespace API_Practice
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int n = 5;
+
+            for (int i = 1; i < n+1; i++)
+            {
+                await GetPokemon(i);
+            }
+        }
+
+        public static async Task GetPokemon(int id)
+        {
+            // Define base url of api.
+            string baseUrl = $"https://pokeapi.co/api/v2/pokemon/{id}";
+       
+            HttpClient client = new HttpClient();
+
+            // Call get request to fetch data from api.
+            HttpResponseMessage response = await client.GetAsync(baseUrl);
+
+            // Extract body from response.
+            string body = await response.Content.ReadAsStringAsync();
+
+            // Parse body into json object.
+            JObject obj = JObject.Parse(body);
+
+            Pokemon p = new Pokemon(obj["id"].Value<int>(), obj["name"].Value<string>(), GetMoves(obj["moves"].Value<JArray>()), obj["weight"].Value<int>(), obj["height"].Value<int>());
+
+            p.print();
+        }
+
+        public static List<string> GetMoves(JArray moves)
+        {
+
+            List<string> strMoves = new List<string>();
+
+            foreach (JObject move in moves) {
+                strMoves.Add(move["move"]["name"].Value<string>());
+            }
+
+            return strMoves;
+        }
+    }
+}
